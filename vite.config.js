f1d3b142046a79f377687b673ca318b72c4833a4
taniboy5514-ops <taniboy5514@gmail.com>
2026-07-28 14:10:20 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-});
+});/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  distDir: 'dist',
+  images: { unoptimized: true }
+}
+module.exports = nextConfig
+
